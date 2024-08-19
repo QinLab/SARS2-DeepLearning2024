f1e9166909629a9aa32622d733da30e038de86ab
Parisa <qxy699@mocs.utc.edu>
2024-08-19 17:54:23 -0400

    add some comments

--- shap/main.py
+++ shap/main.py
@@ -17,16 +17,20 @@ features_tot = []
 var_who = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Omicron']
 
 if __name__ == '__main__':
+    'Data'
+    # Define home directory
     home_dir = os.path.expanduser('~')
     model = tf.keras.models.load_model(f'{home_dir}/{CONST.MODEL_SAVE}')
-
+    
+    # Using df_train for producing base value
     df_train = pd.read_csv(f'{home_dir}/{CONST.TRAIN_DIR}')
     df_test = pd.read_csv(f'{home_dir}/{CONST.TEST_DIR}')
-
-
+    
+    # non_dup_test for calculating SHAP value. We used all of sequences, both train and test sequences, within each variant.
     df_concatenated = pd.concat([df_train, df_test], ignore_index=True)
     non_dup_test = df_concatenated.drop_duplicates(subset=['ID'], keep=False)
     
+    'Calculating base value'
     calc_base = Agg_SHAP(df_train, var)
     
     # Producing base value for calculating of SHAP value for all variants
@@ -43,20 +47,22 @@ if __name__ == '__main__':
     base_value = np.array(explainer.expected_value)
     np.savetxt(f'{CONST.SHAP_DIR}/base_value_{var}.csv', base_value, delimiter=',')
     
+    'RAM needs to be clean'
+    del df_train, df_test, model, df_concatenated, non_dup_test, features_base
+    
+    'Calculating SHAP value for each variant'
     calc_shap = Agg_SHAP(non_dup_test, var)
     features, Ids = calc_shap.get_features( num_seq = None,
                                             ID = None, 
                                             )
-    
-    # RAM needs to be clean
-    del df_train, df_test, model, df_concatenated, non_dup_test, 
-
+ 
     shap_no_zero, non_zero_IDs = cal_shap.get_non_zero_shap_values(
         explainer, 
         features, 
         Id_test,
         )
-
+    
+    'Producing a DataFrame for SHAP values. Each column name represents the position number of a nucleotide in the genetic sequence.'
     df = aggSHAP.get_shap_for_each_nuc(shap_no_zero, non_zero_IDs)
 
     df.loc[f'Total_SHAP_{var}'] = df.sum(numeric_only=True)
