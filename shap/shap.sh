507a01da6fa58299f71e8aa9b99a18bb0b545029
Parisa <qxy699@mocs.utc.edu>
2024-08-21 12:19:28 -0400

    slurm schedualer for shap

---
+++ shap/shap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ 
+# execute in the general partition
+#SBATCH --partition=firefly-full
+ 
+# Number of  processes/tasks
+#SBATCH --ntasks=1
+ 
+# Run all process on a single/multiple node
+#SBATCH --nodes=1
+ 
+# Number of CPU cores per task
+#SBATCH --cpus-per-task=64
+ 
+# time
+#SBATCH --time=3-00:00:00
+ 
+# job name is my_job
+#SBATCH --job-name=SHAP
+ 
+#only use if gpu access required, 2GPUs requested
+#SBATCH --gres=gpu:4
+ 
+# load environment
+module load anaconda
+source activate myenv
+
+export PYTHONPATH="$HOME:$PYTHONPATH"
+# application execution
+python3 ./main.py -var Omicron
