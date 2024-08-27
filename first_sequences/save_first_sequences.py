9468ca0a412b7f5464719331e85a73f081442526
Parisa <qxy699@mocs.utc.edu>
2024-08-27 15:16:48 -0400

    soft code

--- first_sequences/save_first_sequences.py
+++ first_sequences/save_first_sequences.py
@@ -5,10 +5,12 @@ import datetime
 import pandas as pd
 import multiprocessing as mp
 from multiprocessing import current_process, Value
+import sys
+import os
 from tqdm.autonotebook import tqdm
 import time
 
-dir_save = CONST.BATCH_DIR
+dir_save = CONST.FRST_DIR
 
 def save_seq(sequence, filename):
     with open(filename, "w") as handle:
@@ -19,31 +21,31 @@ def read_seq(file_path):
     worker_name = current_process().name    
     sequences = SeqIO.parse(file_path, "fasta")    
     
-    for sequence in tqdm(sequences, desc='Processing sequences'):    
+    for sequence in sequences:    
         id_seq = sequence.id.split('|')
         
         if id_seq[0]=='EPI_ISL_601443':
-            filename = f"{dir_save}/alpha.fasta"
+            filename = f"{dir_save}/alpha_test.fasta"
             save_seq(sequence, filename)
             print(f"{id_seq[0]} is found")
 
         if id_seq[0]=='EPI_ISL_712073':            
-            filename = f"{dir_save}/beta.fasta"
+            filename = f"{dir_save}/beta_test.fasta"
             save_seq(sequence, filename)
             print(f"{id_seq[0]} is found")
 
         if id_seq[0]=='EPI_ISL_2095177':
-            filename = f"{dir_save}/gamma.fasta"
+            filename = f"{dir_save}/gamma_test.fasta"
             save_seq(sequence, filename)
             print(f"{id_seq[0]} is found")
 
         if id_seq[0]=='EPI_ISL_3148365':
-            filename = f"{dir_save}/delta.fasta"
+            filename = f"{dir_save}/delta_test.fasta"
             save_seq(sequence, filename)
             print(f"{id_seq[0]} is found")
 
         if id_seq[0]=='EPI_ISL_6640916':
-            filename = f"{dir_save}/omicron.fasta"
+            filename = f"{dir_save}/omicron_test.fasta"
             save_seq(sequence, filename)
             print(f"{id_seq[0]} is found")
 
@@ -55,9 +57,8 @@ if __name__ == "__main__":
         num_cores = mp.cpu_count()  # Get the number of CPU cores
     else:
         num_cores = CONST.NM_CPU
-    local_found_sequences = set()
-    filenames = glob.glob(f"{dir_save}/group_*.fasta")
-
+#     filenames = glob.glob(f"{dir_save}/group_*.fasta")
+    filenames = glob.glob(f"{CONST.BATCH_DIR}/group_*.fasta")
     with mp.Pool(processes=num_cores) as pool:
         found_sequence = pool.map(read_seq, filenames)
             
