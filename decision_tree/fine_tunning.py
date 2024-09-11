6f9fe43bb5b70d1bf08091a157da1239f24f8482
Parisa <qxy699@mocs.utc.edu>
2024-09-11 12:41:54 -0400

    debugging

--- decision_tree/fine_tunning.py
+++ decision_tree/fine_tunning.py
@@ -1,11 +1,17 @@
+import sys
+import os
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import constants as CONST
 import argparse
 from catboost import CatBoostClassifier
 import json
-import sars.constants as CONST
-from sars.decision_tree.utils import *
+import pandas as pd
+import numpy as np
 import scipy.stats as stats
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from utils import *
 from xgboost import XGBClassifier
 
 
@@ -29,77 +35,76 @@ X_train, y_train = get_data(df_train)
 X_test, y_test = get_data(df_train)
 
 # ------------------------Random forest------------------------
-n_estimators = [int(x) for x in np.linspace(start = 2, stop = 20, num = 10)]
-max_features = ['auto', 'sqrt']
-max_depth = [int(x) for x in np.linspace(10, 50, num = 10)]
-max_depth.append(None)
-min_samples_split = [2, 5, 10]
-min_samples_leaf = [1, 2, 4]
-bootstrap = [True, False]
+# n_estimators = [int(x) for x in np.linspace(start = 2, stop = 20, num = 10)]
+# max_features = ['auto', 'sqrt']
+# max_depth = [int(x) for x in np.linspace(10, 50, num = 10)]
+# max_depth.append(None)
+# min_samples_split = [2, 5, 10]
+# min_samples_leaf = [1, 2, 4]
+# bootstrap = [True, False]
 
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
+# random_grid = {'n_estimators': n_estimators,
+#                'max_features': max_features,
+#                'max_depth': max_depth,
+#                'min_samples_split': min_samples_split,
+#                'min_samples_leaf': min_samples_leaf,
+#                'bootstrap': bootstrap}
 
-rf_model = RandomForestClassifier()
-rf_random = RandomizedSearchCV(estimator = rf_model, 
-                               param_distributions = random_grid, 
-                               n_iter = 100, 
-                               cv = 3, 
-                               verbose=2, 
-                               random_state=42, 
-                               n_jobs = -1,
-                               scoring='accuracy'
-                              )
+# rf_model = RandomForestClassifier()
+# rf_random = RandomizedSearchCV(estimator = rf_model, 
+#                                param_distributions = random_grid, 
+#                                n_iter = 100, 
+#                                cv = 3, 
+#                                verbose=2, 
+#                                random_state=42, 
+#                                n_jobs = -1,
+#                                scoring='accuracy'
+#                               )
 
-rf_random.fit(X_train, y_train)
+# rf_random.fit(X_train, y_train)
 
-best_params_rf = rf_random.best_params_
-with open('./best_params/best_params_rf.json', 'w') as file:
-    json.dump(best_params_rf, file)
+# best_params_rf = rf_random.best_params_
+# with open('./best_params/best_params_rf.json', 'w') as file:
+#     json.dump(best_params_rf, file)
 
-# ------------------------XGBOOST------------------------
-n_estimators = [int(x) for x in np.linspace(start = 2, stop = 20, num = 10)]
-max_depth = [int(x) for x in np.linspace(3, 50, num = 15)]
-max_depth.append(None)
-learning_rate = stats.uniform(0.01, 0.1)
-subsample = stats.uniform(0.5, 0.5)
+# # ------------------------XGBOOST------------------------
+# n_estimators = [int(x) for x in np.linspace(start = 2, stop = 20, num = 10)]
+# max_depth = [int(x) for x in np.linspace(3, 50, num = 15)]
+# max_depth.append(None)
+# learning_rate = stats.uniform(0.01, 0.1)
+# subsample = stats.uniform(0.5, 0.5)
 
-xgb_grid = {
-    'max_depth': max_depth,
-    'learning_rate': learning_rate,
-    'subsample': subsample,
-    'n_estimators':n_estimators
-}
+# xgb_grid = {
+#     'max_depth': max_depth,
+#     'learning_rate': learning_rate,
+#     'subsample': subsample,
+#     'n_estimators':n_estimators
+# }
 
-xgb_model = XGBClassifier()
-xgb_random = RandomizedSearchCV(xgb_model, 
-                                param_distributions=xgb_grid, 
-                                n_iter=10, 
-                                cv=5,
-                                verbose=2,
-                                random_state=42,
-                                n_jobs = -1,
-                                scoring='accuracy',
-                               )
+# xgb_model = XGBClassifier()
+# xgb_random = RandomizedSearchCV(xgb_model, 
+#                                 param_distributions=xgb_grid, 
+#                                 n_iter=10, 
+#                                 cv=5,
+#                                 verbose=2,
+#                                 random_state=42,
+#                                 n_jobs = -1,
+#                                 scoring='accuracy',
+#                                )
 
-xgb_random.fit(X_train, y_train)
+# xgb_random.fit(X_train, y_train)
 
-best_params_xgb = xgb_random.best_params_
-with open('./best_params/best_params_xgb.json', 'w') as file:
-    json.dump(best_params_xgb, file)
+# best_params_xgb = xgb_random.best_params_
+# with open('./best_params/best_params_xgb.json', 'w') as file:
+#     json.dump(best_params_xgb, file)
     
 # ------------------------CatBoost------------------------
-X_train_cat, X_val_cat, y_train_cat, y_val_cat = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-cat_features = list(range(0, X_train.shape[1]))
+# X_train_cat, X_val_cat, y_train_cat, y_val_cat = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
 cat_model = CatBoostClassifier()
 
-iterations = [int(x) for x in np.linspace(start = 10, stop = 1000, num = 10)]
-depth = [int(x) for x in np.linspace(start = 2, stop = 16, num = 5)]
+iterations = [int(x) for x in np.linspace(start = 10, stop = 20, num = 2)]
+depth = [int(x) for x in np.linspace(start = 2, stop = 3, num = 1)]
 learning_rate = stats.uniform(0.01, 0.1)
 cat_grid = {
         'iterations': iterations,
@@ -109,7 +114,7 @@ cat_grid = {
     
 cat_random = RandomizedSearchCV(cat_model,
                                  param_distributions=cat_grid,
-                                 n_iter=10, 
+                                 n_iter=2, 
                                  cv=5,
                                  verbose=2,
                                  random_state=42,
@@ -117,12 +122,9 @@ cat_random = RandomizedSearchCV(cat_model,
                                  scoring='accuracy',
                                  )
 
-cat_random.fit(X_train_cat, y_train_cat, 
-                cat_features=cat_features, 
-                eval_set=(X_val_cat, y_val_cat),
-                verbose=False,
-                )
+cat_random.fit(X_train, y_train)
 
 best_params_cat = cat_random.best_params_
+print("best_params_cat", best_params_cat)
 with open('./best_params/best_params_cat.json', 'w') as file:
     json.dump(best_params_cat, file)
