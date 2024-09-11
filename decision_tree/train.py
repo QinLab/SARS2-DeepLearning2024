36966e6b6915ab8116c94f52043be1b98a60e0bf
Parisa <qxy699@mocs.utc.edu>
2024-09-11 12:41:38 -0400

    debugging

--- decision_tree/train.py
+++ decision_tree/train.py
@@ -1,9 +1,14 @@
+import sys
+import os
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import constants as CONST
 from catboost import CatBoostClassifier
 import joblib
+import json
 import pandas as pd
 from tqdm import trange
-import sars.constants as CONST
-from sars.decision_tree.utils import *
+from utils import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.model_selection import train_test_split
@@ -11,56 +16,60 @@ from xgboost import XGBClassifier
 
 
 # Loading data
-df_train = pd.read_csv(CONST.TRAIN_DT)
-df_test = pd.read_csv(CONST.TEST_DT)
+df_train = pd.read_csv(CONST.TRAIN_DT)[:20]
+df_test = pd.read_csv(CONST.TEST_DT)[:20]
 
 # Prepare data
 X_train, y_train = get_data(df_train)
 X_test, y_test = get_data(df_train)
 
-# Random forest
-model = RandomForestClassifier(max_depth=2, random_state=0)
+# ------------------------Random forest------------------------
+with open("./best_params/best_params_rf.json", "r") as f:
+    best_param_rf = json.load(f)
+model = RandomForestClassifier(**best_param_rf)
 model.fit(X_train, y_train)
 joblib.dump(model, CONST.MODEL_RF, compress=3)
 
-y_preds = model.predict(x_test)
-accuracy = accuracy_score(y_test, predictions)
+y_preds = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_preds)
 print("Accuracy in random forest: %.2f%%" % (accuracy * 100.0))
 
 plot_confusion_matrix('rf', y_preds, y_test, 'red', 'Reds' )
-plot_metrics('rf', y_preds, y_test, color, 'Reds')
+plot_metrics('rf', y_preds, y_test, 'Reds')
 
-# XGBoost
-model = XGBClassifier()
+# ------------------------XGBoost------------------------
+with open("./best_params/best_params_xgb.json", "r") as f:
+    best_params_xgb = json.load(f)
+model = XGBClassifier(**best_params_xgb)
 model.fit(x_train, y_train)
 joblib.dump(model, CONST.MODEL_XGB, compress=3)
 
-y_pred = model.predict(x_test)
-predictions = [round(value) for value in y_pred]
+y_pred = model.predict(X_test)
+# predictions = [round(value) for value in y_pred]
 
-accuracy = accuracy_score(y_test, predictions)
+accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy in XGBoost: %.2f%%" % (accuracy * 100.0))
 
 plot_confusion_matrix('xgb', y_preds, y_test, 'green', 'Greens' )
-plot_metrics('xgb', y_preds, y_test, color, 'Greens')
+plot_metrics('xgb', y_preds, y_test, 'Greens')
 
-# CatBoost
+# ------------------------CatBoost------------------------
 X_train_cat, X_val_cat, y_train_cat, y_val_cat = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-cat_features = list(range(0, X_train.shape[1]))
 
+# with open("./best_params/best_params_cat.json", "r") as f:
+#     best_params_cat = json.load(f)
 model = CatBoostClassifier()
 model.fit(X_train_cat, y_train_cat, 
-        cat_features=cat_features, 
         eval_set=(X_val_cat, y_val_cat),
         verbose=False
 )
 joblib.dump(model, CONST.MODEL_CTB, compress=3)
 
-y_pred = model.predict(x_test)
-predictions = [round(value) for value in y_pred]
+y_pred = model.predict(X_test)
+# predictions = [round(value) for value in y_pred]
 
-accuracy = accuracy_score(y_test, predictions)
+accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy in CatBoost: %.2f%%" % (accuracy * 100.0))
 
 plot_confusion_matrix('cat', y_preds, y_test, 'orange', 'Oranges' )
-plot_metrics('cat', y_preds, y_test, color, 'Oranges')
+plot_metrics('cat', y_preds, y_test, 'Oranges')
