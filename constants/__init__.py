ea12aa28de12cae6dcb199d5a469dbbc617937b8
Parisa <qxy699@mocs.utc.edu>
2024-09-30 20:50:54 -0400

    update constants

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,8 +1,8 @@
 from .constants import LEN_SIZE, NM_CPU, SEQ_SIZE, SPLIT_RATIO, VOC_WHO
 from .constants import CHARS, CONDONTAB, AMINO_ACID, BALANC_DIR, BATCH_DIR 
 from .constants import BST_PARAM_DIR, FRST_DIR, HST_DIR
-from .constants import LABEL_DIR, MODEL_CTB, MODEL_RF, MODEL_SAVE, MODEL_XGB
-from .constants import ORf_DIR, PVLU_DIR, RSLT_DIR, SEQ_DIR
+from .constants import LABEL_DIR, MODEL_CTB, MODEL_RF, MODEL_SAVE, MODEL_XGB, MUT_DIR
+from .constants import ORf_DIR, PVLU_DIR, PVLU_DIR2, RSLT_DIR, SEQ_DIR
 from .constants import SHAP_DIR, TRAIN_DIR, TRAIN_DT, TEST_DIR, TEST_DT 
 
 
@@ -25,8 +25,10 @@ __all__ = [
     "MODEL_RF",
     "MODEL_SAVE",
     "MODEL_XGB",
+    "MUT_DIR",
     "ORf_DIR",
     "PVLU_DIR", 
+    "PVLU_DIR2",
     "RSLT_DIR",
     "SEQ_DIR",
     "SHAP_DIR",
